27a5bab1e29e32e4a471abea9c46241c5f6e38bf
BoltsTheBoss <moobothelp@gmail.com>
2015-09-27 23:55:44 -0400

    Create diceroll.php

---
+++ src/Bolts/diceroll.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bolts\main;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase {
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        
+       if($sender instanceof Player)
+           $number = mt_rand(1, 6);
+            if ($number == 1) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 1.");
+                return;
+            }
+            if ($number == 2) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 2.");
+                return;
+            }
+            if ($number == 3) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 3.");
+                return;
+            }
+            if ($number == 4) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 4.");
+                return;
+            }
+            if ($number == 5) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 5.");
+                return;
+            }
+            if ($number == 6) {
+                $sender->sendMessage(Color::BLUE."[Server] You rolled a ... 6.");
+                return;
+            }
+    }
+}    
